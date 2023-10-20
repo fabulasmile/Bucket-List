cda80d57d5a84439110b8fc1a793c3f2ffcc2cc3
dimastri <aretira23@gmail.com>
2023-10-20 16:52:41 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+    
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form["num_give"]
+    db.bucket.update_one(
+        {'num': int(num_receive)},
+        {'$set': {'done': 1}}
+    )
+    return jsonify({'msg': 'Update done!'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    buckets_list = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'buckets':buckets_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
